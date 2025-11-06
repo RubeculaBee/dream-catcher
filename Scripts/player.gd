f80e6328b2ec799c116bb170bca067241c6625dd
Robin Lane <rubeculabee@gmail.com>
2025-11-05 19:13:15 -0800

    movement direction is now handled in a vector 2

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
-var movementHoriz: float
-var movementVert: float
+@onready var sprite: AnimatedSprite2D = find_child("AnimatedSprite2D")
 
 func _process(_delta: float) -> void:
-	movementHoriz = Input.get_axis("left", "right")
-	movementVert = Input.get_axis("up", "down")
+	var movement_direction: Vector2i = get_movement()
+	print(movement_direction)
 
-	print("test")
-	print("Horiz: %s | Vert: %s" % [movementHoriz, movementVert])
+	
+func get_movement() -> Vector2i:
+	var dir: Vector2
+	dir.x = Input.get_axis("left", "right")
+	dir.y = Input.get_axis("up", "down")
+
+	return dir
