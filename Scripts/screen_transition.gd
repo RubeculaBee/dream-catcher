b36f37a25a893bd9b57f66cc24cafcc21b2b2434
Robin Lane <rubeculabee@gmail.com>
2025-11-07 17:47:11 -0800

    reworked player collisnbox

--- Scripts/screen_transition.gd
+++ Scripts/screen_transition.gd
@@ -2,3 +2,9 @@ extends Area2D
 class_name ScreenTransition
 
 @export var next_screen: PackedScene
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area: Area2D) -> void:
+	print("I, %s, have been entered by %s!" % [self, area])
