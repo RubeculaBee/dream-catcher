8f6cde100f3617742da3e4fc76a28c443a33006e
Robin Lane <rubeculabee@gmail.com>
2025-11-08 22:34:48 -0800

    reorganised game_manager.gd methods

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -4,24 +4,31 @@ extends Node
 signal player_move_response(response: bool) ## A signal designed to tell the player whether it can move
 
 # Nodes
+var current_room: Node
 var terrain: TileMapLayer
 var player: Player
 var screen_transitions: Array
 
 func _ready() -> void:
-	player = find_child("Player")
+	current_room = get_node("Rooms").get_child(0)
+
+	player = current_room.get_node("Player")
 	player.tried_move.connect(_on_player_tried_move) # connect game_manager to player's signal
 	player_move_response.connect(Callable(player, "_on_move_response")) # connect player to game_manager's signal
-	
-	terrain = find_child("Terrain Tile Map")
+
+	update_references()
+
+func update_references() -> void:
+	terrain = current_room.get_node("Terrain Tile Map")
 	assert(terrain != null, "Terrain not found!")
 
-	screen_transitions = find_child("Screen Transitions").get_children()
+	screen_transitions = current_room.get_node("Screen Transitions").get_children()
 	for i in screen_transitions.size():
 		for j in range(i+1, screen_transitions.size()):
 			assert(screen_transitions[i].id != screen_transitions[j].id, "Duplicate ID in screen transition triggers!")
 		
 		screen_transitions[i].transition.connect(_on_transition)
+
 
 func _on_player_tried_move(tile: Vector2i) -> void:
 	var tile_data: TileData = terrain.get_cell_tile_data(tile)
@@ -30,13 +37,9 @@ func _on_player_tried_move(tile: Vector2i) -> void:
 	player_move_response.emit(valid_move)
 
 func _on_transition(transition: ScreenTransition):
-	print("Entered:", transition.id)
-	var current_room: Node = get_node("Rooms").get_child(0)
-	var next_room: Node = transition.next_screen.instantiate()
-
-	player.reparent(next_room)
-	terrain = next_room.get_node("Terrain Tile Map")
-	screen_transitions = next_room.get_node("Screen Transitions").get_children()
-	
-	current_room.add_sibling.call_deferred(next_room)
 	current_room.queue_free()
+	current_room = transition.next_screen.instantiate()
+	get_node("Rooms").add_child.call_deferred(current_room)
+
+	player.reparent(current_room)
+	update_references()
