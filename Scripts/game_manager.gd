fa97da92e6d50488e18340ecfdb92fcb9074505f
Robin Lane <rubeculabee@gmail.com>
2025-11-08 21:24:46 -0800

    game manager sees when transition is entered

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -17,12 +17,17 @@ func _ready() -> void:
 	assert(terrain != null, "Terrain not found!")
 
 	screen_transitions = get_parent().find_child("Screen Transitions").get_children()
-	for i in screen_transitions.size()-1:
+	for i in screen_transitions.size():
 		for j in range(i+1, screen_transitions.size()):
 			assert(screen_transitions[i].id != screen_transitions[j].id, "Duplicate ID in screen transition triggers!")
+		
+		screen_transitions[i].transition.connect(_on_transition)
 
 func _on_player_tried_move(tile: Vector2i) -> void:
 	var tile_data: TileData = terrain.get_cell_tile_data(tile)
 	var valid_move: bool = (tile_data != null) && (tile_data.get_custom_data("walkable"))
 
 	player_move_response.emit(valid_move)
+
+func _on_transition(transition: ScreenTransition):
+	print("Entered:", transition.id)
--- Scripts/screen_transition.gd
+++ Scripts/screen_transition.gd
@@ -1,11 +1,16 @@
 extends Area2D
 class_name ScreenTransition
 
+# Variables
 @export var next_screen: PackedScene
 @export var id: int
+
+# Signal 
+signal transition(transtion: ScreenTransition)
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
-	print("I, %s, have been entered by %s!" % [self, area])
+	if area.name == "PlayerCollision":
+		transition.emit(self)
