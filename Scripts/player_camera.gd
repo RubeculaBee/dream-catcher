f4d16420fa931cd8ab51569110f4a716817623a3
Robin Lane <rubeculabee@gmail.com>
2025-12-10 20:08:11 -0800

    camera now follows player through transitions instntly instead of smoothely

--- Scripts/player_camera.gd
+++ Scripts/player_camera.gd
@@ -6,10 +6,13 @@ class_name PlayerCamera
 
 #signals
 signal screen_covered ## Emit when the screen is fully covered
+signal screen_uncovered ## Emit when the transition is done
 
 func _ready() -> void:
 	# Turn off screen cover at start
 	screen_cover.color.a = 0;
+	screen_covered.connect(_on_screen_covered)
+	screen_uncovered.connect(_on_screen_uncovered)
 
 func fade_transition() -> void:
 	var tween: Tween = create_tween()
@@ -18,6 +21,7 @@ func fade_transition() -> void:
 	tween.tween_property(screen_cover, "color", Color(0, 0, 0, 1), 0.25)
 	tween.tween_callback(screen_covered.emit)
 	tween.tween_property(screen_cover, "color", Color(0, 0, 0, 0), 0.25)
+	tween.tween_callback(screen_uncovered.emit)
 
 func swipe_transition() -> void:
 	screen_cover.position = Vector2(800, -screen_cover.size.y/2)
@@ -32,3 +36,10 @@ func swipe_transition() -> void:
 
 	# tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(screen_cover, "position", Vector2(-screen_cover.size.x*2, -screen_cover.size.y/2), 0.4)
+	tween.tween_callback(screen_uncovered.emit)
+
+func _on_screen_covered():
+	position_smoothing_enabled = false
+
+func _on_screen_uncovered():
+	position_smoothing_enabled = true
