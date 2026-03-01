8359427dd20dcf3c0fb30e3779e5da000ef96124
Robin Lane <rubeculabee@gmail.com>
2026-03-01 15:35:49 -0800

    move class is now abstract and has subclasses for specific moves

---
+++ Scripts/Figments/figmentTester.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _ready():
+	Move.LandAttack.new().doEffect(null, null)
--- Scripts/Figments/move.gd
+++ Scripts/Figments/move.gd
@@ -1,22 +1,22 @@
-extends Node
+@abstract
 class_name Move
 
 var title: String
 var accuracy: float
 var type: Figment.Type
-var effect: Variant #should be a function
 
-func _init(t: String, a: float, ty: Figment.Type, e: Variant) -> void:
-	title = t
-	accuracy = a
-	type = ty
-	effect = e
+@abstract
+func doEffect(user: Figment, target: Figment)
 
-static var moveList: Dictionary = {
-	"landAttack": Move.new(
-		"Land Attack",
-		1,
-		Figment.Type.LAND,
-		func(): print("land attack")
-	)
-}
+class LandAttack:
+	extends Move
+
+	func _init() -> void:
+		title = "Land Attack"
+		accuracy = 1
+		type = Figment.Type.LAND
+
+	func doEffect(user: Figment, target: Figment):
+		print("land attack!")
+
+
