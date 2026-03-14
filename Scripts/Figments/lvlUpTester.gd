3c6172b4aae49708b44df36ce5672a888d13bf8e
Robin Lane <rubeculabee@gmail.com>
2026-03-13 19:15:53 -0700

    level up tester is now functional

--- Scripts/Figments/lvlUpTester.gd
+++ Scripts/Figments/lvlUpTester.gd
@@ -6,11 +6,31 @@ var indicators: Array[Array]
 
 func _ready() -> void:
 	figment = Figment.new("seaTest")
-	$Figment.texture = figment.sprite
-	$Level.text = "Level: %s" % figment.level
+	lShards = 10
 
 	setupLvlIndicator()
 	connectSignals()
+	updateLevels()
+	updateFigment()
+
+func updateFigment():
+	$Figment.texture = figment.sprite
+	$Figment/Name.text = figment.speciesName
+	for row: Array[ColorRect] in indicators:
+		for ind: ColorRect in row:
+			ind.color = Color.GRAY
+
+
+func updateLevels():
+	$Level.text = "Level: %s" % figment.level
+	$LShards.text = "LShards: %s" % lShards
+	for button: Button in $Stats.get_children():
+		button.get_child(0).text = "lvl: %s" % figment.stats[button.text.to_lower()].level
+		button.get_child(1).text = "value: %s\ngrowth: %s" %[
+			figment.stats[button.text.to_lower()].value,
+			figment.stats[button.text.to_lower()].growth
+		]
+
 
 func connectSignals():
 	$Stats/Button.pressed.connect(levelUp.bind(0))
@@ -18,11 +38,29 @@ func connectSignals():
 	$Stats/Button3.pressed.connect(levelUp.bind(2))
 	$Stats/Button4.pressed.connect(levelUp.bind(3))
 	$Stats/Button5.pressed.connect(levelUp.bind(4))
+	$LShards/Button.pressed.connect(printShards)
+
+	$Figment/Regenerate/Land.pressed.connect(newFigment.bind("landTest"))
+	$Figment/Regenerate/Sea.pressed.connect(newFigment.bind("seaTest"))
+	$Figment/Regenerate/Sky.pressed.connect(newFigment.bind("skyTest"))
+
+func newFigment(bp: String):
+	figment = Figment.new(bp)
+	updateLevels()
+	updateFigment()
+
+func printShards():
+	lShards += 5
+	updateLevels()
 
 func levelUp(statNum: int):
-	var statname: String = figment.stats.keys()[statNum]
-	indicators[statNum][figment.stats[statname].level].color = Color.YELLOW
-	figment.levelUp(statname)
+	if lShards > 0:
+		var statname: String = figment.stats.keys()[statNum]
+		if figment.levelUp(statname):
+			indicators[statNum][figment.stats[statname].level-1].color = Color.YELLOW
+			lShards-=1
+			updateLevels()
+		
 
 func setupLvlIndicator():
 	for j in range(5):
