a4fa75cd8973ee443409a0da8011e0fd8a8f0937
Robin Lane <rubeculabee@gmail.com>
2026-04-16 15:29:57 -0700

    background flips for smoother looping

--- Scripts/Objects/background.gd
+++ Scripts/Objects/background.gd
@@ -5,16 +5,17 @@ var isClone: bool = false
 func _ready() -> void:
 	assert(texture != null, "Background has no texture!")
 	if !isClone:
-		copy(800)
+		copy(800, true)
 
-func copy(distance: int):
+func copy(distance: int, flip: bool):
 	var dupe = self.duplicate()
 	dupe.position.x += distance
+	dupe.flip_h = flip
 	dupe.isClone = true
 	add_sibling.call_deferred(dupe)
 
 func _process(delta: float) -> void:
-	position.x -= 32 * delta
+	position.x -= 128 * delta
 	if position.x < -799:
-		copy(1599)
+		copy(1599, self.flip_h)
 		queue_free()
