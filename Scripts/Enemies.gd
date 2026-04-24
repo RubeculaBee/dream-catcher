4e37dcdfc5e4bd7550f869d5891a1043800cb269
carmel-hod <carmel.n.hod@gmail.com>
2026-04-23 17:08:44 -0700

    multiple enmemies and spawnpoints for them

--- Scripts/Enemies.gd
+++ Scripts/Enemies.gd
@@ -1,16 +1,26 @@
 extends Node2D
 class_name Enemies
 
+var TILE_SIZE: int = 32 
+
 @export var enemy_scene : PackedScene
 @onready var player = get_parent().find_child("Player")
 
-func spawn_enemy(world_pos: Vector2) -> Node2D:
+@export var numEnemies : int
+@export var spawnpoints : Array[Vector2i]
+var spawnpointsNow : Array[Vector2i]
+
+func _ready() -> void:
+	spawnpointsNow = spawnpoints
+
+func spawn_enemy() -> Node2D:
 	if enemy_scene == null:
 		push_error("Enemies node has no enemy_scene assigned!")
 		return null
-
+	var spawn = randi_range(0,spawnpointsNow.size()-1)
 	var enemy = enemy_scene.instantiate()
-	enemy.position = world_pos
+	enemy.position = Vector2(spawnpoints[spawn] * TILE_SIZE)
+	spawnpoints.remove_at(spawn)
 	enemy.alias = str(randi_range(0,1000))
 	add_child(enemy)
 	return enemy
